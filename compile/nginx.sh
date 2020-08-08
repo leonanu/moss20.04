15c2adbb55e9be94388962136202c97d1ba9b5d2
leonanu <nanu@inanu.net>
2020-08-08 19:59:03 +0800

    update Nginx

--- compile/nginx.sh
+++ compile/nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-## nginx-1.10.x
+## nginx-1.19.x
 if ! grep '^NGINX$' ${INST_LOG} > /dev/null 2>&1 ;then
 
 ## check proc
@@ -13,35 +13,37 @@ if ! grep '^NGINX$' ${INST_LOG} > /dev/null 2>&1 ;then
     get_file
     unpack
 
-    ### get nginx vim files
-    [ -d "${STORE_DIR}/${SRC_DIR}/contrib/vim" ] && cp -rf ${STORE_DIR}/${SRC_DIR}/contrib/vim ${STORE_DIR}/nginx_vim
-
     CONFIG="./configure --prefix=${INST_DIR}/${SRC_DIR} \
-    --pid-path=/var/run/nginx.pid \
-    --error-log-path=/var/log/nginx/error.log \
-    --http-log-path=/var/log/nginx/access.log \
-    --http-client-body-temp-path=/usr/local/nginx/var/tmp/client_body \
-    --http-proxy-temp-path=/usr/local/nginx/var/tmp/proxy \
-    --http-fastcgi-temp-path=/usr/local/nginx/var/tmp/fastcgi \
-    --http-uwsgi-temp-path=/usr/local/nginx/var/tmp/uwsgi \
-    --with-threads \
-    --with-file-aio \
-    --with-http_ssl_module \
-    --with-http_v2_module \
-    --with-http_realip_module \
-    --with-http_addition_module \
-    --with-http_image_filter_module \
-    --with-http_gunzip_module \
-    --with-http_gzip_static_module \
-    --with-http_auth_request_module \
-    --with-http_stub_status_module \
-    --with-pcre \
-    --without-http_ssi_module \
-    --without-http_geo_module \
-    --without-http_scgi_module \
-    --without-mail_pop3_module \
-    --without-mail_imap_module \
-    --without-mail_smtp_module"
+            --pid-path=/var/run/nginx.pid \
+            --error-log-path=/var/log/nginx/error.log \
+            --http-log-path=/var/log/nginx/access.log \
+            --http-client-body-temp-path=/usr/local/nginx/var/tmp/client_body \
+            --http-proxy-temp-path=/usr/local/nginx/var/tmp/proxy \
+            --http-fastcgi-temp-path=/usr/local/nginx/var/tmp/fastcgi \
+            --http-uwsgi-temp-path=/usr/local/nginx/var/tmp/uwsgi \
+            --with-threads \
+            --with-file-aio \
+            --with-http_ssl_module \
+            --with-http_v2_module \
+            --with-http_flv_module \
+            --with-http_mp4_module \
+            --with-http_realip_module \
+            --with-http_addition_module \
+            --with-http_gunzip_module \
+            --with-http_gzip_static_module \
+            --with-http_auth_request_module \
+            --with-http_stub_status_module \
+            --with-pcre \
+            --with-stream \
+            --with-stream_ssl_module \
+            --with-stream_realip_module \
+            --with-stream_ssl_preread_module \
+            --without-http_ssi_module \
+            --without-http_geo_module \
+            --without-http_scgi_module \
+            --without-mail_pop3_module \
+            --without-mail_imap_module \
+            --without-mail_smtp_module"
 
 ## for compile
     MAKE='make'
@@ -50,48 +52,39 @@ if ! grep '^NGINX$' ${INST_LOG} > /dev/null 2>&1 ;then
     compile
     
     [ ! -d "${INST_DIR}/${SRC_DIR}/conf/vhosts" ] && mkdir -m 0755 -p ${INST_DIR}/${SRC_DIR}/conf/vhosts
+    [ ! -d "${INST_DIR}/${SRC_DIR}/conf/stream" ] && mkdir -m 0755 -p ${INST_DIR}/${SRC_DIR}/conf/stream
 
 ## for install config files
     succ_msg "Begin to install ${SRC_DIR} config files"
-    ## user add
-    id www >/dev/null 2>&1 || useradd www -u 1001 -M -s /sbin/nologin
     ## document root
     [ ! -d "${NGX_DOCROOT}" ] && mkdir -m 0755 -p ${NGX_DOCROOT}
-    chown -R www:www ${NGX_DOCROOT}
+    chown -R www-data:www-data ${NGX_DOCROOT}
     ## log directory
     [ ! -d "${NGX_LOGDIR}" ] && mkdir -m 0755 -p ${NGX_LOGDIR}
-    chown -R www:www ${NGX_LOGDIR}
+    chown -R www-data:www-data ${NGX_LOGDIR}
     ## tmp directory
     [ ! -d "/usr/local/nginx/var/tmp/client_body" ] && mkdir -m 0777 -p /usr/local/nginx/var/tmp/client_body
     [ ! -d "/usr/local/nginx/var/tmp/fastcgi" ] && mkdir -m 0777 -p /usr/local/nginx/var/tmp/fastcgi
     [ ! -d "/usr/local/nginx/var/tmp/uwsgi" ] && mkdir -m 0777 -p /usr/local/nginx/var/tmp/uwsgi
     [ ! -d "/usr/local/nginx/var/tmp/proxy" ] && mkdir -m 0777 -p /usr/local/nginx/var/tmp/proxy
-    chown -R www:www /usr/local/nginx/var
+    chown -R www-data:www-data /usr/local/nginx/var
     ## conf
     install -m 0644 ${TOP_DIR}/conf/nginx/nginx.conf ${INST_DIR}/${SRC_DIR}/conf/nginx.conf
     install -m 0644 ${TOP_DIR}/conf/nginx/vhost.conf ${INST_DIR}/${SRC_DIR}/conf/vhosts/${NGX_HOSTNAME}.conf
-    sed -i "s#.*ng_server_name.*#    server_name  ${NGX_HOSTNAME};#" ${INST_DIR}/${SRC_DIR}/conf/vhosts/${NGX_HOSTNAME}.conf
-    sed -i "s#.*ng_root.*#    root         ${NGX_DOCROOT};#" ${INST_DIR}/${SRC_DIR}/conf/vhosts/${NGX_HOSTNAME}.conf
-    sed -i "s#.*ng_access_log.*#    access_log  ${NGX_LOGDIR}/${NGX_HOSTNAME}_access.log main buffer=4k;#" ${INST_DIR}/${SRC_DIR}/conf/vhosts/${NGX_HOSTNAME}.conf
-    sed -i "s#.*ng_error_log.*#    error_log   ${NGX_LOGDIR}/${NGX_HOSTNAME}_error.log error;#" ${INST_DIR}/${SRC_DIR}/conf/vhosts/${NGX_HOSTNAME}.conf
-    ## Vim syntax hilight
-    VIM_SHARE_DIR=$(ls /usr/share/vim | grep 'vim7')
-    [ ! -d "/usr/share/vim/${VIM_SHARE_DIR}/ftdetect" ] && mkdir -m 0755 -p /usr/share/vim/${VIM_SHARE_DIR}/ftdetect
-    [ ! -d "/usr/share/vim/${VIM_SHARE_DIR}/indent" ] && mkdir -m 0755 -p /usr/share/vim/${VIM_SHARE_DIR}/indent
-    [ ! -d "/usr/share/vim/${VIM_SHARE_DIR}/syntax" ] && mkdir -m 0755 -p /usr/share/vim/${VIM_SHARE_DIR}/syntax
-    cp -rf ${STORE_DIR}/nginx_vim/ftdetect/* /usr/share/vim/${VIM_SHARE_DIR}/ftdetect/
-    cp -rf ${STORE_DIR}/nginx_vim/indent/* /usr/share/vim/${VIM_SHARE_DIR}/indent/
-    cp -rf ${STORE_DIR}/nginx_vim/syntax/* /usr/share/vim/${VIM_SHARE_DIR}/syntax/
-    chown -R root.root /usr/share/vim/${VIM_SHARE_DIR}/
-    rm -rf ${STORE_DIR}/nginx_vim
+    install -m 0644 ${TOP_DIR}/conf/nginx/stream_http.conf.example ${INST_DIR}/${SRC_DIR}/conf/stream/stream_http.conf.example
+    install -m 0644 ${TOP_DIR}/conf/nginx/stream_https.conf.example ${INST_DIR}/${SRC_DIR}/conf/stream/stream_https.conf.example
+    sed -i "s#.*ng_server_name.*#    server_name  ${NGX_HOSTNAME};#g" ${INST_DIR}/${SRC_DIR}/conf/vhosts/${NGX_HOSTNAME}.conf
+    sed -i "s#.*ng_root.*#    root         ${NGX_DOCROOT};#g" ${INST_DIR}/${SRC_DIR}/conf/vhosts/${NGX_HOSTNAME}.conf
+    sed -i "s#.*ng_access_log.*#    access_log  ${NGX_LOGDIR}/${NGX_HOSTNAME}_access.log main buffer=4k;#g" ${INST_DIR}/${SRC_DIR}/conf/vhosts/${NGX_HOSTNAME}.conf
+    sed -i "s#.*ng_error_log.*#    error_log   ${NGX_LOGDIR}/${NGX_HOSTNAME}_error.log error;#g" ${INST_DIR}/${SRC_DIR}/conf/vhosts/${NGX_HOSTNAME}.conf
     ## log
     [ ! -d "/usr/local/etc/logrotate" ] && mkdir -m 0755 -p /usr/local/etc/logrotate
     install -m 0644 ${TOP_DIR}/conf/nginx/nginx.logrotate /usr/local/etc/logrotate/nginx
     sed -i "s#/var/log/nginx#${NGX_LOGDIR}#g" /usr/local/etc/logrotate/nginx
     [ ! -d "${INST_DIR}/${SRC_DIR}/logs" ] && mkdir -m 0755 -p ${INST_DIR}/${SRC_DIR}/logs
     [ ! -d "/var/log/nginx" ] && mkdir -m 0755 -p /var/log/nginx
-    chown -R www:www ${INST_DIR}/${SRC_DIR}/logs
-    chown -R www:www /var/log/nginx
+    chown -R www-data:www-data ${INST_DIR}/${SRC_DIR}/logs
+    chown -R www-data:www-data /var/log/nginx
     ## cron job
     echo '' >> /var/spool/cron/root
     echo '# Logrotate - Nginx' >> /var/spool/cron/root
--- conf/nginx/ca.sh
+++ conf/nginx/ca.sh
@@ -7,8 +7,9 @@ if [ -e "/usr/local/nginx/conf/ssl/moss.pem" ];then
 else
     PRE_VHOST_DN=www.example.com
     umask 77
-    openssl req -utf8 -newkey rsa:2048 -keyout /usr/local/nginx/conf/ssl/moss.key -nodes -x509 -days 1825 -out /usr/local/nginx/conf/ssl/moss.crt -set_serial 0 -subj "/C=CN/ST=Moss/L=Moss/O=Moss Inc/CN=$VHOST_DN"
+    openssl req -utf8 -newkey rsa:2048 -keyout /usr/local/nginx/conf/ssl/moss.key -nodes -x509 -days 3650 -out /usr/local/nginx/conf/ssl/moss.crt -set_serial 0 -subj "/C=CN/ST=Moss/L=Moss/O=Moss Inc/CN=$VHOST_DN"
     cat /usr/local/nginx/conf/ssl/moss.key >  /usr/local/nginx/conf/ssl/moss.pem
     echo ""                           >> /usr/local/nginx/conf/ssl/moss.pem
     cat /usr/local/nginx/conf/ssl/moss.crt >> /usr/local/nginx/conf/ssl/moss.pem
+    openssl dhparam -out /usr/local/nginx/conf/ssl/dh2048.pem 2048
 fi
